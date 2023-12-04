7b941e6eeaf0d70c902062061dbcd89ab5206177
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-12-04 18:54:18 -0300

    Initialize semana08-30-misc-cookies

---
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -0,0 +1,47 @@
+package br.edu.utfpr.cp.espjava.crudcidades;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/").hasAnyRole("listar", "admin")
+                .requestMatchers("/criar", "/excluir", "/preparaAlterar", "/alterar").hasRole("admin")
+                .requestMatchers("/mostrar").authenticated()
+                .anyRequest().denyAll()
+                .and()
+                .formLogin()
+                .loginPage("/login.html").permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder cifrador() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @EventListener(InteractiveAuthenticationSuccessEvent.class)
+    public void printUsuarioAtual(InteractiveAuthenticationSuccessEvent event) {
+
+        var usuario = event.getAuthentication().getName();
+
+        System.out.println(usuario);
+    }
+}
