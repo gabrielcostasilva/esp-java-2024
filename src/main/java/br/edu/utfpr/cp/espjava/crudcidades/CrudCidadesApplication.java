31c0b534fea525f03b830777d86a28414325c5df
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-12-04 18:54:09 -0300

    Initialize semana04-30-validacao-javax-completa

---
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
@@ -0,0 +1,33 @@
+package br.edu.utfpr.cp.espjava.crudcidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import jakarta.validation.Validator;
+
+@SpringBootApplication
+public class CrudCidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCidadesApplication.class, args);
+	}
+
+	@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+	@Bean
+    public Validator getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
