bb6087d68d8bf585e96ab9f3029a51d926792f75
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-12-04 18:54:12 -0300

    Initialize semana05-30-integracao-conversao-dto

---
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/cidade/CidadeEntidade.java
@@ -0,0 +1,42 @@
+package br.edu.utfpr.cp.espjava.crudcidades.cidade;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity (name = "cidade")
+public class CidadeEntidade implements Serializable{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String estado;
+    
+    public String getEstado() {
+        return estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
